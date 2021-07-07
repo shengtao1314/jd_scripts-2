7e7f59983a39af12145c3a13e7e65c867f8b60e2
sempliciy <sempliciy@gmail.com>
2021-07-07 08:57:19 +0800

    京豆雨

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -127,3 +127,7 @@
 0 */1 * * * node /scripts/jd_joy_park.js >> /scripts/logs/jd_joy_park.log 2>&1
 # 财富岛提现
 0 0 * * * node /scripts/jx_cfdtx.js >> /scripts/logs/jx_cfdtx.log 2>&1
+# 半点京豆雨
+30 16-23/1 * * * node /scripts/jd_half_redrain.js >> /scripts/logs/jd_half_redrain.log 2>&1
+# 整点京豆雨
+1 0-23/1 * * * node /scripts/jd_super_redrain.js >> /scripts/logs/jd_super_redrain.log 2>&1
